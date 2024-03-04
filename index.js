4bfc9586f4f85844fc1fe510a4874cc84e74fa2c
shuragir <149947204+shuragir@users.noreply.github.com>
2024-03-04 21:00:02 +0000

    Added new files

---
+++ index.js
@@ -0,0 +1,43 @@
+class ShuraginsTea {
+    constructor() {
+      this.teaData = {};
+    }
+  
+    setTeaData(key, value) {
+      this.teaData[key] = value;
+      console.log(`Set Shuragins Tea data for key '${key}':`, value);
+    }
+  
+    getTeaData(key) {
+      const value = this.teaData[key];
+      console.log(`Retrieved Shuragins Tea data for key '${key}':`, value);
+      return value;
+    }
+  
+    brewTea() {
+      console.log('Brewing a delightful cup of Shuragins Tea. ☕');
+    }
+  
+    customizeBlend(blend) {
+      console.log(`Customizing Shuragins Tea blend with ${blend}.`);
+    }
+  
+    addMagicIngredient(ingredient) {
+      console.log(`Adding a magical touch of ${ingredient} to enhance Shuragins Tea flavor.`);
+    }
+  
+    enjoyTea() {
+      console.log('Sipping on the enchanting flavor of Shuragins Tea. 🍵✨');
+    }
+  }
+  
+  // Example usage:
+  const shuraginsTea = new ShuraginsTea();
+  
+  shuraginsTea.setTeaData('flavor', 'Mystical Elixir');
+  shuraginsTea.setTeaData('aroma', 'Enchanting Essence');
+  shuraginsTea.brewTea();
+  shuraginsTea.customizeBlend('Forest Blossom');
+  shuraginsTea.addMagicIngredient('Stardust');
+  shuraginsTea.enjoyTea();
+  
